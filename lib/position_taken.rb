25c9aaf9efd7432446dddaadd44e43e6b423c070
kitte anderson <kitteanderson@gmail.com>
2018-09-26 16:36:49 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 
   #another way to write position_taken?
-  def position_taken?(board, index = X)
+  def position_taken?(board, index = X || O)
     if board[index] == " " || "" || nil
       return false
     else
